af34c276b793884915ee176683f040d44f2732be
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2020-05-23 14:35:08 -0700

    Rename 3d Printing Protocol Buffers (#36)
    Resolves #35

---
+++ client/printing_3d/print_3d_grpc_pb.js
@@ -0,0 +1,44 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('grpc');
+var print_3d_pb = require('./print_3d_pb.js');
+
+function serialize_Print3dRequest(arg) {
+  if (!(arg instanceof print_3d_pb.Print3dRequest)) {
+    throw new Error('Expected argument of type Print3dRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_Print3dRequest(buffer_arg) {
+  return print_3d_pb.Print3dRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_Print3dResponse(arg) {
+  if (!(arg instanceof print_3d_pb.Print3dResponse)) {
+    throw new Error('Expected argument of type Print3dResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_Print3dResponse(buffer_arg) {
+  return print_3d_pb.Print3dResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var Printer3dService = exports.Printer3dService = {
+  handle3dPrint: {
+    path: '/Printer3d/Handle3dPrint',
+    requestStream: false,
+    responseStream: false,
+    requestType: print_3d_pb.Print3dRequest,
+    responseType: print_3d_pb.Print3dResponse,
+    requestSerialize: serialize_Print3dRequest,
+    requestDeserialize: deserialize_Print3dRequest,
+    responseSerialize: serialize_Print3dResponse,
+    responseDeserialize: deserialize_Print3dResponse,
+  },
+};
+
+exports.Printer3dClient = grpc.makeGenericClientConstructor(Printer3dService);
--- server/printing_3d/print_3d_server.py
+++ server/printing_3d/print_3d_server.py
@@ -10,22 +10,25 @@ import os
 from os import sep
 
 
-class Printer3DServicer(print_3d_pb2_grpc.Printer3DServicer):
+class Printer3dServicer(print_3d_pb2_grpc.Printer3dServicer):
     CUURENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__)) + sep
     proc = None
 
-    def Print(self, request, context):
-        response = print_3d_pb2.PrintResponse()
+    def Handle3dPrint(self, request, context):
+        response = print_3d_pb2.Print3dResponse()
         response.message = 'the pi thinks you are cool'
         return response
-    
+
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    print_3d_pb2_grpc.add_Printer3DServicer_to_server(Printer3DServicer(), server)
+    print_3d_pb2_grpc.add_Printer3dServicer_to_server(
+        Printer3dServicer(), server)
     print('Starting server. Listening on port 50051.')
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     logging.basicConfig()
