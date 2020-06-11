84967bdf10952f0c9acd1602619c2f25c369d6cb
Lawrence Nguyen <43452418+law-nguyen@users.noreply.github.com>
2020-06-10 17:28:54 -0700

    add eslint styling. Resolves #42 (#45)

--- client/printing_3d/print_3d_client.js
+++ client/printing_3d/print_3d_client.js
@@ -1,19 +1,19 @@
 const grpc = require('grpc');
-var messages = require('./print_3d_pb');
-var services = require('./print_3d_grpc_pb');
+const messages = require('./print_3d_pb');
+const services = require('./print_3d_grpc_pb');
 
 function send3dPrintRequest(raw, name, volume, copies) {
-    let client = new services.Printer3dClient('localhost:50053',
-        grpc.credentials.createInsecure());
-    let request = new messages.Print3dRequest();
-    request.setCopies(copies);
-    request.setMemberName(name);
-    request.setEncodedFile(raw);
-    request.setPrintVolumeCubicCm(volume);
-    client.handle3dPrint(request, function (err, response) {
-        if (err) console.log(err);
-        console.log('Message:', response.getMessage());
-    });
+  let client = new services.Printer3dClient('localhost:50053',
+    grpc.credentials.createInsecure());
+  let request = new messages.Print3dRequest();
+  request.setCopies(copies);
+  request.setMemberName(name);
+  request.setEncodedFile(raw);
+  request.setPrintVolumeCubicCm(volume);
+  client.handle3dPrint(request, function(err, response) {
+    if (err) console.log(err);
+    console.log('Message:', response.getMessage());
+  });
 }
 
-module.exports = { send3dPrintRequest }
+module.exports = { send3dPrintRequest };
