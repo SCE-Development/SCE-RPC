162f647ce0130a3f59e4fe6d2271c4033df2fa10
nanarboursal <55638619+nanarboursal@users.noreply.github.com>
2020-06-22 13:11:31 -0700

    AddMessageToQueue and ClearMessageQueue (#47)
    * Added AddMessageToQueue and ClearMessageQueue RPC and regenerated python files
    
    * Removed exe file and fixed client and server files
    
    * Fixed merge conflict
    
    Co-authored-by: Nanar Boursalian <nanar.boursalian01@gmail.com>

--- client/ledsign/led_sign_grpc_pb.js
+++ client/ledsign/led_sign_grpc_pb.js
@@ -4,6 +4,17 @@
 var grpc = require('grpc');
 var led_sign_pb = require('./led_sign_pb.js');
 
+function serialize_LedSignMessage(arg) {
+  if (!(arg instanceof led_sign_pb.LedSignMessage)) {
+    throw new Error('Expected argument of type LedSignMessage');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_LedSignMessage(buffer_arg) {
+  return led_sign_pb.LedSignMessage.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 function serialize_LedSignRecord(arg) {
   if (!(arg instanceof led_sign_pb.LedSignRecord)) {
     throw new Error('Expected argument of type LedSignRecord');
@@ -15,38 +26,16 @@ function deserialize_LedSignRecord(buffer_arg) {
   return led_sign_pb.LedSignRecord.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_LedSignRequest(arg) {
-  if (!(arg instanceof led_sign_pb.LedSignRequest)) {
-    throw new Error('Expected argument of type LedSignRequest');
-  }
-  return Buffer.from(arg.serializeBinary());
-}
-
-function deserialize_LedSignRequest(buffer_arg) {
-  return led_sign_pb.LedSignRequest.deserializeBinary(new Uint8Array(buffer_arg));
-}
-
-function serialize_LedSignResponse(arg) {
-  if (!(arg instanceof led_sign_pb.LedSignResponse)) {
-    throw new Error('Expected argument of type LedSignResponse');
-  }
-  return Buffer.from(arg.serializeBinary());
-}
-
-function deserialize_LedSignResponse(buffer_arg) {
-  return led_sign_pb.LedSignResponse.deserializeBinary(new Uint8Array(buffer_arg));
-}
-
 
 var LedSignService = exports.LedSignService = {
   healthCheck: {
     path: '/LedSign/HealthCheck',
     requestStream: false,
     responseStream: false,
-    requestType: led_sign_pb.LedSignRequest,
+    requestType: led_sign_pb.LedSignMessage,
     responseType: led_sign_pb.LedSignRecord,
-    requestSerialize: serialize_LedSignRequest,
-    requestDeserialize: deserialize_LedSignRequest,
+    requestSerialize: serialize_LedSignMessage,
+    requestDeserialize: deserialize_LedSignMessage,
     responseSerialize: serialize_LedSignRecord,
     responseDeserialize: deserialize_LedSignRecord,
   },
@@ -55,11 +44,33 @@ var LedSignService = exports.LedSignService = {
     requestStream: false,
     responseStream: false,
     requestType: led_sign_pb.LedSignRecord,
-    responseType: led_sign_pb.LedSignResponse,
+    responseType: led_sign_pb.LedSignMessage,
     requestSerialize: serialize_LedSignRecord,
     requestDeserialize: deserialize_LedSignRecord,
-    responseSerialize: serialize_LedSignResponse,
-    responseDeserialize: deserialize_LedSignResponse,
+    responseSerialize: serialize_LedSignMessage,
+    responseDeserialize: deserialize_LedSignMessage,
+  },
+  addMessageToQueue: {
+    path: '/LedSign/AddMessageToQueue',
+    requestStream: false,
+    responseStream: false,
+    requestType: led_sign_pb.LedSignMessage,
+    responseType: led_sign_pb.LedSignMessage,
+    requestSerialize: serialize_LedSignMessage,
+    requestDeserialize: deserialize_LedSignMessage,
+    responseSerialize: serialize_LedSignMessage,
+    responseDeserialize: deserialize_LedSignMessage,
+  },
+  clearMessageQueue: {
+    path: '/LedSign/ClearMessageQueue',
+    requestStream: false,
+    responseStream: false,
+    requestType: led_sign_pb.LedSignMessage,
+    responseType: led_sign_pb.LedSignMessage,
+    requestSerialize: serialize_LedSignMessage,
+    requestDeserialize: deserialize_LedSignMessage,
+    responseSerialize: serialize_LedSignMessage,
+    responseDeserialize: deserialize_LedSignMessage,
   },
 };
 
