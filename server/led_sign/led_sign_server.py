d18e5534c81e5b8ea3b96813343c354e30227aa0
Seema Vora <seema.s.vora@gmail.com>
2020-07-25 21:34:40 -0700

    Oops Forgot to Save File

--- server/led_sign/led_sign_server.py
+++ server/led_sign/led_sign_server.py
@@ -72,13 +72,6 @@ def serve():
     server.wait_for_termination()
     visual = LedSignMock()
 
-    # main (){
-    #     const testRequest = {text: 'i dont think this willwork'};
-
-
-    # }
-
 if __name__ == '__main__':
     logging.basicConfig()
     serve()
-
