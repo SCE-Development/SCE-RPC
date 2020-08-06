d83075c4e8c01014056d56772b3ee2627a94190b
Seema Vora <seema.s.vora@gmail.com>
2020-08-06 11:13:47 -0700

    Requests from RPC Recognized

--- server/led_sign/LedSignMock.py
+++ server/led_sign/LedSignMock.py
@@ -22,6 +22,9 @@ class LedSignMock():
     'Current Border Color: ' ]
     ESCKEY = 27
 
+    def getSignText(self, text):
+        print(text)
+
     def update_sign_visual(self, request):
         self.sign_data["text"] = request["text"]
         self.sign_data["brightness"] = str(request["brightness"])
