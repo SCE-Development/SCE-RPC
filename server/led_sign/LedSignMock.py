ace56263d2347f7630039f06b640e5e898b206c9
Seema Vora <seema.s.vora@gmail.com>
2020-08-06 13:44:36 -0700

    Works! Except for One Error

--- server/led_sign/LedSignMock.py
+++ server/led_sign/LedSignMock.py
@@ -15,6 +15,8 @@ class LedSignMock():
         "border-color":''
     }
 
+    screen = curses.initscr()
+
     menu = ['   ','SCE LED Sign Visualization ', ' ','Current Text: ', 'Current Brightness: ',' ',
     'Current Scroll Speed:', 'Current Background Color: ', 'Current Font Color:',
     'Current Border Color: ' ]
@@ -27,47 +29,47 @@ class LedSignMock():
         self.sign_data["text"] = request["text"]
         print(self.sign_data["text"], 'ok cool')
         print('surabhi is a genius')
-        self.sign_data["brightness"] = request["brightness"]
-        self.sign_data["scroll-speed"] = request["scroll_speed"]
-        self.sign_data["background-color"] = request["background_color"]
-        self.sign_data["font-color"] = request["text_color"]
-        self.sign_data["border-color"] = request["border_color"]
+        self.sign_data["brightness"] = str(request["brightness"])
+        self.sign_data["scroll-speed"] = str(request["scroll-speed"])
+        self.sign_data["background-color"] = request["background-color"]
+        self.sign_data["font-color"] = request["font-color"]
+        self.sign_data["border-color"] = request["border-color"]
+        self.print_menu(0)
+        self.screen.refresh()
 
     def print_menu(self, selected_row_idx):
-        screen = curses.initscr()
-        screen.clear()
-        screen.border(0)
-        height, width = screen.getmaxyx()
-        print('wooooo')
+        self.screen.clear()
+        self.screen.border(0)
+        # print(self.sign_data["text"], 'print')
+        height, width = self.screen.getmaxyx()
         for idx, row in enumerate(self.menu): #iterates over different menu options and aligns it
             x = width//3 - len(self.menu) 
             #calculates the location of the words (division changed by # of things in self.menu)
             y = height//3 - len(self.menu)//3 + idx
-            screen.addstr(y, x, row)
+            self.screen.addstr(y, x, row)
             if idx == 3:
-                screen.addstr(y ,x +25, self.sign_data["text"])
+                self.screen.addstr(y ,x +25, self.sign_data["text"])
             elif idx == 4:
-                screen.addstr(y,x+25,self.sign_data["brightness"])
+                self.screen.addstr(y,x+25,self.sign_data["brightness"])
             elif idx == 6:
-                screen.addstr(y ,x +25,self.sign_data["scroll-speed"])
+                self.screen.addstr(y ,x +25,self.sign_data["scroll-speed"])
             elif idx == 7:
-                screen.addstr(y,x +25,self.sign_data["background-color"])
+                self.screen.addstr(y,x +25,self.sign_data["background-color"])
             elif idx == 8:
-                screen.addstr(y,x +25,self.sign_data["font-color"])
+                self.screen.addstr(y,x +25,self.sign_data["font-color"])
             elif idx == 9:
-                screen.addstr(y,x +25,self.sign_data["border-color"])
-        screen.refresh()
+                self.screen.addstr(y,x +25,self.sign_data["border-color"])
+        self.screen.refresh()
 
     def creates_display(self):
-        screen = curses.initscr()
         curses.curs_set(0)
         current_row = 0
         self.print_menu(current_row)
-        checkKey = screen.getch()
+        checkKey = self.screen.getch()
         while checkKey != self.ESCKEY:
-            checkKey = screen.getch()
+            checkKey = self.screen.getch()
         curses.endwin()
         exit()
-        self.print_menu(current_row)
-        print("updates mock")
+        # self.print_menu(current_row)
+        # print("updates mock")
 
